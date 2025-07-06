1cf6f08080e1c263569c7b84fdcc9cbe06fd6baf
itz_govind <govind.sirsath@adoptnet.tech>
2025-07-06 16:00:56 +0530

    added logging framework to application

--- authservice/src/main/java/admin/myapp/com/authservice/AuthserviceApplication.java
+++ authservice/src/main/java/admin/myapp/com/authservice/AuthserviceApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthserviceApplication.class, args);
-		System.out.println("***** Auth Service Application Started Successfully ******");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthserviceApplication.class, args);
+        System.out.println("\n********************************Auth Service Application Started Successfully********************************");
+    }
 
 }
